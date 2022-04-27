8b35603c157088334f490050115db4322db2e8fd
missystem <misy.shi@gmail.com>
2022-04-27 01:44:37 -0500

    passed all tests

--- views/post_likes.py
+++ views/post_likes.py
@@ -1,7 +1,8 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import LikePost, db
+from models import LikePost, db, Post
 import json
+from views import can_view_post
 
 class PostLikesListEndpoint(Resource):
 
@@ -9,10 +10,40 @@ class PostLikesListEndpoint(Resource):
         self.current_user = current_user
     
     def post(self):
-        # create a new "like_post" based on the data posted in the body 
+        # TODO: create a new "like_post" based on the data posted in the body 
+        ## Create a like
+        ## post_id: int (required)
+        ## LikePost object
         body = request.get_json()
         print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+        # ------------------------ CODE START HERE ------------------------ #
+        ## Check if input did not meet required 1 itemss:
+        # if len(body) < 1:
+        #     return Response(json.dumps({}), mimetype="application/json", status=400)
+        ## Check if post_id is int:
+        try:
+            post_id = int(body.get('post_id'))
+        except:
+            return Response(json.dumps({}), mimetype="application/json", status=400)
+        ## Check if post exists in all posts
+        if Post.query.get(post_id) is None:
+            return Response(json.dumps([]), mimetype="application/json", status=404)
+        ## Check if the user is authorized (followed by the current user)
+        ## Use can_view_post()
+        if not can_view_post(post_id, self.current_user):
+            return Response(json.dumps([]), mimetype="application/json", status=404)
+        ## Check if post_id exists
+        lp_query = LikePost.query.filter_by(user_id=self.current_user.id).all()
+        lp_post_ids = [lp.post_id for lp in lp_query]
+        if post_id in lp_post_ids:
+            return Response(json.dumps([]), mimetype="application/json", status=400)
+
+        user_id = self.current_user.id
+        liked = LikePost (user_id, post_id)
+        db.session.add(liked)
+        db.session.commit()
+        # ----------------------------------------------------------------- #
+        return Response(json.dumps(liked.to_dict()), mimetype="application/json", status=201)
 
 class PostLikesDetailEndpoint(Resource):
 
@@ -21,8 +52,18 @@ class PostLikesDetailEndpoint(Resource):
     
     def delete(self, id):
         # delete "like_post" where "id"=id
+        ## Delete a like
+        ## You can ONLY delete a "like" that you created
         print(id)
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        # ------------------------ CODE START HERE ------------------------ #
+        lp_query = LikePost.query.filter_by(user_id=self.current_user.id).all()
+        lp_ids = [lp.id for lp in lp_query]
+        if id in lp_ids:
+            LikePost.query.filter_by(id=id).delete()
+            db.session.commit()
+            return Response(json.dumps([]), mimetype="application/json", status=200)
+        # ----------------------------------------------------------------- #
+        return Response(json.dumps({}), mimetype="application/json", status=404)
 
 
 
