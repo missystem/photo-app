21746d9e2f7a3fa3a56912efb6741bbbc00f7efc
missystem <misy.shi@gmail.com>
2022-05-02 13:46:14 -0500

    update

--- views/suggestions.py
+++ views/suggestions.py
@@ -5,10 +5,11 @@ from views import get_authorized_user_ids
 import json
 import random
 
-# Credits: 
+# Credits:
 # Remove all the elements that occur in one list from another
 # https://stackoverflow.com/questions/4211209/remove-all-the-elements-that-occur-in-one-list-from-another
-# 
+#
+
 
 class SuggestionsListEndpoint(Resource):
 
@@ -44,7 +45,7 @@ class SuggestionsListEndpoint(Resource):
         # ----------------------------------------
         return Response(json.dumps(suggestions), mimetype="application/json", status=200)
     """
-    
+
     def get(self):
         # TODO: suggestions should be any user with an ID that's not in this list:
         ## OPTIMIZED VERSION
@@ -52,6 +53,7 @@ class SuggestionsListEndpoint(Resource):
         ## use the User data model to get this information
         ## just display 7 users that the current user isn't already following
         # ---------------------- code start here -----------------------
+        ## all users that current user is following
         user_ids = get_authorized_user_ids(self.current_user)
         ## query 7 users from all users but exclude user_ids (followings)
         suggestions = User.query.filter(~User.id.in_(user_ids)).limit(7).all()
@@ -62,8 +64,8 @@ class SuggestionsListEndpoint(Resource):
 
 def initialize_routes(api):
     api.add_resource(
-        SuggestionsListEndpoint, 
-        '/api/suggestions', 
-        '/api/suggestions/', 
+        SuggestionsListEndpoint,
+        '/api/suggestions',
+        '/api/suggestions/',
         resource_class_kwargs={'current_user': api.app.current_user}
     )
