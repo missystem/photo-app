5a87f4531db0c0dce8ec039f3f0ee921aabad280
missystem <misy.shi@gmail.com>
2022-05-23 22:50:55 -0500

    post_likes is fixed

--- views/post_likes.py
+++ views/post_likes.py
@@ -3,12 +3,14 @@ from flask_restful import Resource
 from models import LikePost, db, Post
 import json
 from views import can_view_post
+import flask_jwt_extended
 
 class PostLikesListEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @flask_jwt_extended.jwt_required()
     def post(self):
         # TODO: create a new "like_post" based on the data posted in the body 
         ## Create a like
@@ -48,6 +50,7 @@ class PostLikesDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
     
+    @flask_jwt_extended.jwt_required()
     def delete(self, id):
         ## DELETE "like_post" where "id"=id
         ## You can ONLY delete a "like" that you created
@@ -72,12 +75,12 @@ def initialize_routes(api):
         PostLikesListEndpoint, 
         '/api/posts/likes', 
         '/api/posts/likes/', 
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
 
     api.add_resource(
         PostLikesDetailEndpoint, 
         '/api/posts/likes/<int:id>', 
         '/api/posts/likes/<int:id>/',
-        resource_class_kwargs={'current_user': api.app.current_user}
+        resource_class_kwargs={'current_user': flask_jwt_extended.current_user}
     )
